96fd4b7801705091eb80020ae4458faed271a658
seri <seri.wb@gmail.com>
2017-01-16 02:08:58 +0900

    create tool menu

--- src/main/groovy/white/box/sclone/Main.groovy
+++ src/main/groovy/white/box/sclone/Main.groovy
@@ -4,18 +4,12 @@ import static groovyx.javafx.GroovyFX.start
 
 start {
 
-    def fxml = new javafx.fxml.FXMLLoader(this.class.getResource('/sclone.fxml'))
-    stage(title: 'GroovyFX Hello World', visible: true) {
-        scene(root: fxml.load(), fill: BLACK, width: 600, height: 250) {
-            hbox(padding: 60) {
-                text(text: 'Groovy', font: '80pt sanserif') {
-                    fill linearGradient(endX: 0, stops: [PALEGREEN, SEAGREEN])
-                }
-                text(text: 'FX', font: '80pt sanserif') {
-                    fill linearGradient(endX: 0, stops: [CYAN, DODGERBLUE])
-                    effect dropShadow(color: DODGERBLUE, radius: 25, spread: 0.25)
-                }
-            }
+    def titleBarName = "sclone 1"
+
+    stage(title: titleBarName, visible: true) {
+        scene(stylesheets: resource("/sclone.css")) {
+            fxml(resource('/sclone.fxml'))
         }
     }
+    primaryStage.resizable = false
 }
